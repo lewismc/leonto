22db7152bffed6544dd6eeee4574a7ed15e5410f
lewismc <lewis.mcgibbney@gmail.com>
2013-02-14 18:46:26 -0800

    trivial update

--- src/java/uk/ac/gcu/ebe/legalontoconstruct/ConstructJob.java
+++ src/java/uk/ac/gcu/ebe/legalontoconstruct/ConstructJob.java
@@ -3,9 +3,13 @@
  */
 package uk.ac.gcu.ebe.legalontoconstruct;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
 
 import org.apache.any23.Any23;
 import org.apache.any23.extractor.ExtractionException;
@@ -22,8 +26,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * The main method in {@link ConstructJob} simply accepts one 'url' input 
+ * parameter, from which we extract triples.
  * @author lewismc
- *
  */
 public class ConstructJob implements Tool {
 	
@@ -63,8 +68,9 @@ public class ConstructJob implements Tool {
 		HTTPClient httpClient = runner.getHTTPClient();
 		DocumentSource source = null;
 	  source = new HTTPDocumentSource(httpClient, u);
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		TripleHandler handler = new NTriplesWriter(out);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		// TODO make the writer switch configurable 
+		TripleHandler handler = new NTriplesWriter(baos);
 		try {
 		log.info("ConstructJob: Extracting from: '" + u + "'");
 		runner.extract(source, handler);
@@ -73,7 +79,15 @@ public class ConstructJob implements Tool {
     } finally {
 		handler.close();
 		}
-		String n3 = out.toString("UTF-8");
+		String out = baos.toString("UTF-8");
+		try {
+			BufferedWriter bf = new BufferedWriter(new FileWriter("construct.txt"));
+			bf.write(out);
+			bf.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		log.info("ConstructorJob: Finished!");
 		return 0;
 	}
 	public Configuration getConf() {
