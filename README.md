0ad527b33198d584b20cb4256240709a40b66e50
Lewis John McGibbney <lewismc@apache.org>
2014-03-12 18:00:58 +0000

    Update to leonto project

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
-leonto README
-=================
+`7MMF'      `7MM"""YMM    .g8""8q. `7MN.   `7MF'MMP""MM""YMM   .g8""8q.   
+  MM          MM    `7  .dP'    `YM. MMN.    M  P'   MM   `7 .dP'    `YM. 
+  MM          MM   d    dM'      `MM M YMb   M       MM      dM'      `MM 
+  MM          MMmmMM    MM        MM M  `MN. M       MM      MM        MM 
+  MM      ,   MM   Y  , MM.      ,MP M   `MM.M       MM      MM.      ,MP 
+  MM     ,M   MM     ,M `Mb.    ,dP' M     YMM       MM      `Mb.    ,dP' 
+.JMMmmmmMMM .JMMmmmmMMM   `"bmmd"' .JML.    YM     .JMML.      `"bmmd"' 
 
 @author: lewismc
 contact: lewismc [at] apache [dot] org
 
-Leonto presents a framework for regulatory ontology construction from complex 
-legislative documentation within the AEC Domain.
+Leonto (Legal Ontology Framework) presents a framework for achieving the 
+construction of regulatory ontologies from complex legislative documentation 
+specifically from within the AEC (architecture engineering and construction) 
+Domain.
+
+This product includes software developed by The Apache Software
+Foundation (http://www.apache.org/).
+
+Predominantly this product uses software developed by the Apache Any23 
+project (http://any23.apache.org).
--- src/java/uk/ac/gcu/ebe/legalontoconstruct/ConstructJob.java
+++ src/java/uk/ac/gcu/ebe/legalontoconstruct/ConstructJob.java
@@ -3,99 +3,71 @@
  */
 package uk.ac.gcu.ebe.legalontoconstruct;
 
+
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
 
 import org.apache.any23.Any23;
 import org.apache.any23.extractor.ExtractionException;
-import org.apache.any23.http.HTTPClient;
 import org.apache.any23.source.DocumentSource;
-import org.apache.any23.source.HTTPDocumentSource;
+import org.apache.any23.writer.JSONWriter;
 import org.apache.any23.writer.NTriplesWriter;
 import org.apache.any23.writer.TripleHandler;
 import org.apache.any23.writer.TripleHandlerException;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 /**
- * The main method in {@link ConstructJob} simply accepts one 'url' input 
- * parameter, from which we extract triples.
+ * <p>The main method in {@link ConstructJob} simply accepts one 'file' input 
+ * parameter, from which we extract triples.</p>
+ * <p>If the class is invoked without any parameters it will display a
+ * useage message.</p>
  * @author lewismc
  */
-public class ConstructJob implements Tool {
-	
-	private static final Logger log = LoggerFactory.getLogger(ConstructJob.class);
+public class ConstructJob {
 
-	
-	public static void main(String [] args) throws Exception{
-		final int res = ToolRunner.run(new ConstructJob(), args);
-		System.exit(res);
-	}
-	private Configuration conf;
-	/**
-	 * @param args
-	 */
-	public int run(String[] args) throws Exception{
-		String url = null;
-		String usage = "Usage: ConstructJob -url <url>";
-		
-		if(args.length < 1) {
-			System.err.println(usage);
-			return -1;
-		}
-		for(int i = 0; i < args.length; i++) {
-			if("-url".equals(args[i])){
-				getConf().set("seed.url", args[i++]);
-			}
-			url = args[i];
-		}
-		return runner(url);
-	}
-	
-	public int runner(String url) throws IOException, TripleHandlerException, URISyntaxException {
-		String u = url;
-		log.info("ConstructJob: Starting...");
-		Any23 runner = new Any23();
-		runner.setHTTPUserAgent("<==legalOntoConstruct==>");
-		HTTPClient httpClient = runner.getHTTPClient();
-		DocumentSource source = null;
-	  source = new HTTPDocumentSource(httpClient, u);
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		// TODO make the writer switch configurable 
-		TripleHandler handler = new NTriplesWriter(baos);
-		try {
-		log.info("ConstructJob: Extracting from: '" + u + "'");
-		runner.extract(source, handler);
-		} catch (ExtractionException e) {
-	    e.printStackTrace();
-    } finally {
-		handler.close();
-		}
-		String out = baos.toString("UTF-8");
-		try {
-			BufferedWriter bf = new BufferedWriter(new FileWriter("construct.txt"));
-			bf.write(out);
-			bf.close();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-		log.info("ConstructorJob: Finished!");
-		return 0;
-	}
-	public Configuration getConf() {
-	  return conf;
+  public static void main(String [] args) throws Exception{
+    String filePath = null;
+
+    if(args.length < 2) {
+      System.err.println("Usage: ConstructJob -file </path/to/file>");
+    }
+    for(int i = 0; i < args.length; i++) {
+      if("-file".equals(args[i])){
+        try {
+          filePath = args[++i];
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      } runner(filePath);
+    }
   }
-	public void setConf(Configuration conf) {
-	  this.conf = conf;
-	  
+
+  public static void runner(String fileURIString) throws IOException, TripleHandlerException, URISyntaxException {
+
+    Any23 runner = new Any23();
+    DocumentSource source = runner.createDocumentSource("file:" + fileURIString);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    TripleHandler handler = new JSONWriter(baos);
+    try {
+      runner.extract(source, handler);
+    } catch (ExtractionException e) {
+      e.printStackTrace();
+    } finally {
+      handler.close();
+    }
+    String out = baos.toString("UTF-8");
+    try {
+      BufferedWriter bf = new BufferedWriter(new FileWriter("construct.txt"));
+      bf.write(out);
+      bf.close();
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
   }
 
 }
